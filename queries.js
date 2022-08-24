dc0dd70669670cb1b8208cb0e78c2da71fb46b02
Nathan Clevenger <nateclev@gmail.com>
2022-08-24 06:39:45 -0500

    Update queries.js

--- queries.js
+++ queries.js
@@ -2,4 +2,5 @@ const db = {}
 
 export default {
   deleteEdge: id => db.prepare('DELETE FROM edges WHERE source = ? OR target = ?').bind(id),
+  deleteNode: id => db.prepare('DELETE FROM nodes WHERE id = ?').bind(id),
 }
