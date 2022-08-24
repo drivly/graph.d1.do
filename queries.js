c0082c9a468722bde627d3ca0bcc1bed40fdd80e
Nathan Clevenger <nateclev@gmail.com>
2022-08-24 06:53:42 -0500

    Update queries.js

--- queries.js
+++ queries.js
@@ -8,4 +8,8 @@ export default {
   searchEdgesInbound: id => db.prepare('SELECT * FROM edges WHERE source = ?').bind(id),
   searchEdgesOutbound: id => db.prepare('SELECT * FROM edges WHERE target = ?').bind(id),
   searchEdges: id => db.prepare('SELECT * FROM edges WHERE source = ? UNION SELECT * FROM edges WHERE target = ?').bind(id),
+  searchNodeById: id => db.prepare('SELECT body FROM nodes WHERE id = ?').bind(id),
+  traverseInbound: id => db.prepare('WITH RECURSIVE traverse(id) AS (SELECT :source UNION SELECT source FROM edges JOIN traverse ON target = id) SELECT id FROM traverse;').bind(id),
+  traverseOutbound: id => db.prepare('WITH RECURSIVE traverse(id) AS (SELECT :source UNION SELECT target FROM edges JOIN traverse ON source = id) SELECT id FROM traverse;').bind(id),
+  traverseWithBodiesInbound: (x, y, obj) => db.prepare(`WITH RECURSIVE traverse(x, y, obj) AS (SELECT :source, '()', '{}' UNION SELECT id, '()', body FROM nodes JOIN traverse ON id = x UNION SELECT source, '<-', properties FROM edges JOIN traverse ON target = x) SELECT x, y, obj FROM traverse;`).bind(x, y, obj),
 }
