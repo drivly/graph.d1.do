f1bb718794a302d0170b4dee8647d4ddcc8d9d7b
Nathan Clevenger <nateclev@gmail.com>
2022-08-24 06:43:27 -0500

    Update queries.js

--- queries.js
+++ queries.js
@@ -5,4 +5,6 @@ export default {
   deleteNode: id => db.prepare('DELETE FROM nodes WHERE id = ?').bind(id),
   insertEdge: (source, target, obj) => db.prepare('INSERT INTO edges VALUES(?, ?, json(?))').bind(source, target, obj),
   insertNode: (obj) => db.prepare('INSERT INTO nodes VALUES(json(?))').bind(obj),
+  searchEdgesInbound: id => db.prepare('SELECT * FROM edges WHERE source = ?').bind(id),
+  searchEdgesOutbound: id => db.prepare('SELECT * FROM edges WHERE target = ?').bind(id),
 }
