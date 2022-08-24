4b2d4157d93063dcc2df8921994fbc6653f07e96
Nathan Clevenger <nateclev@gmail.com>
2022-08-24 06:40:58 -0500

    Update queries.js

--- queries.js
+++ queries.js
@@ -3,4 +3,5 @@ const db = {}
 export default {
   deleteEdge: id => db.prepare('DELETE FROM edges WHERE source = ? OR target = ?').bind(id),
   deleteNode: id => db.prepare('DELETE FROM nodes WHERE id = ?').bind(id),
+  insertEdge: (source, target, obj) => db.prepare('INSERT INTO edges VALUES(?, ?, json(?))').bind(source, target, obj),
 }
